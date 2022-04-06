260fe87f80cb5f4f5f81360d6c65cb601040ba58
samuel guedes <guedes.works7@gmail.com>
2022-04-06 00:13:03 -0400

    feat: apply SSG concept at home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage, GetServerSideProps } from 'next';
+import type { NextPage, GetServerSideProps, GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -40,7 +40,7 @@ const Home: NextPage<HomeProps> = ({ product }) => {
 
 export default Home;
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KlJ7wA52gCsivIqzWzMnlUT');
 
   const formatedPrice = new Intl.NumberFormat('en-US', {
@@ -56,6 +56,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
